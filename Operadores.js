62b50664d31ce6d884f4a5de57d3c2bd396e667e
mramos.dev423 <mramosg@fcpn.edu.bo>
2023-04-21 22:07:59 -0400

    se añadio conceptos en el archivo de Operadores.js

---
+++ Operadores.js
@@ -0,0 +1,39 @@
+//!¿Qué son los operadores?
+//! Operadores de asignación
+let w=5 //La variable x se asigna con el valor 5
+// `+=`,`-=`,`+=`,`/=`,`%=`
+let z=10
+z+=5 // z=z+5// z=10+5 
+  // z=15 
+ let c=4
+ c-=3                      
+console.log(suma);
+console.log(resta);
+//!Operadores aritméticos
+var x = 5;
+var y = 2;
+var suma = x + y; // La variable suma tiene un valor de 7
+var resta = x - y; // La variable resta tiene un valor de 3
+var multiplicacion = x * y; // La variable multiplicacion tiene un valor de 10
+var division = x / y; // La variable division tiene un valor de 2.5
+var modulo = x % y; // La variable modulo tiene un valor de 1
+//!Operador de incremento
+console.log('Operador de incremento');
+//Pre-incremento
+let cont=0;
+console.log(++cont) //cont=cont+1 //cont=0+1//cont=1
+console.log('cont ',cont);
+//Post-Incremento
+let cont1=0;
+console.log(cont1++)
+console.log('cont1',cont1)
+//!Operador de decremento
+console.log('Operador de incremento');
+//Pre-incremento
+let contR=0;
+console.log(--contR) //cont=cont-1 //cont=0-1//cont=-1
+console.log('cont ',contR);
+//Post-Incremento
+let contR1=0;
+console.log(contR1--)
+console.log('cont1',contR1)
