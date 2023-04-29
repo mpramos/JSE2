9283e0138f152f40587e43e8634b795145813634
mramos.dev423 <mramosg@fcpn.edu.bo>
2023-04-29 11:03:15 -0400

    arreglos y funciones

--- Arreglos.js
+++ Arreglos.js
@@ -1,3 +1,11 @@
+const arr= Array() 
+console.log(arr);
+
+let ochoElementos=Array(8).fill('🎈')
+console.log(ochoElementos);
+
+const arr1=[]
+console.log(arr1);
 
 let cajon1='🎃'
 
@@ -17,10 +25,97 @@ console.log(fiesta);
 console.log(sillas.length);
 console.log(sillas[0]);//2
 let sillas= ['👨','🎊','😷']//3
-for (let i = 0; i <= sillas.length-1; i++) { //4
-    console.log(sillas[i]);
+for (let posicion = 0; posicion <= sillas.length-1; posicion++) { //4
+    console.log(sillas[posicion]);
 }
 
+let cocina1=['🍊','🍓','🍕','🍠','🍊','🍓','🍕','🍠','🍊','🍓','🍕','🍕'] 
+console.log(cocina1);
+cocina1[2]='🍋'
+console.log(cocina1);
+console.log(cocina1[3]);
+//POSICION 3
+// TAMAÑO 4
+console.log(cocina1.length)
+let ultimaPosicion=cocina1.length-1
+console.log(ultimaPosicion)
+console.log(cocina1[ultimaPosicion]);
+
+let comidaChatarra=['🍕','🍟','🍕','🍕','🍟','🍕','🍕','🍟','🍔','🍕','🥩','🥝','🥩']
+
+let comidaSaludable=['🍋','🍓','🥝']
+
+let cocina3=comidaChatarra.concat(comidaSaludable)
+console.log(cocina3);
+console.log(comidaChatarra.indexOf('🍔'));
+
+console.log(comidaChatarra.indexOf('🥩'));
+if (comidaChatarra.indexOf('🥩')===-1) {
+    console.log(`El elemento 🥩  no esta dentro de mi coleccion`);
+}else{
+    console.log(`El elemento 🥩  esta dentro de mi coleccion`);
+}
+//TODO indice está guardando la posicion del filete
+let indice=comidaChatarra.indexOf('🥩')
+console.log(indice);
+
+indice === -1 ? console.log('El elemento 🥩  no esta dentro de mi coleccion ?'):console.log(`El elemento 🥩  esta dentro de mi coleccion ?`);
+
+let dispositivos=['💻','📲','🧮','👨🏻‍💻','🧮']
+
+let primeraPosicionElemento= dispositivos.indexOf('🍟')
+console.log(primeraPosicionElemento);
+let ultimaPosicionElemento= dispositivos.lastIndexOf('🍟')
+console.log(ultimaPosicionElemento);
+primeraPosicionElemento ===-1 ? console.log('no existe'):console.log('si existe');
+ultimaPosicionElemento ===-1 ? console.log('no existe'):console.log('si existe');
+console.log(dispositivos.includes('🍟')) // false
+console.log(dispositivos.includes('💻')) // false
+
+let frutas=['🍓','🍋','🍊']
+console.log(Array.isArray(frutas));
+let frutas1= 100
+console.log(Array.isArray(frutas1));
+
+// toString
+const numeros=[1,2,3,4,5]
+console.log(numeros);
+console.log(numeros.toString());
+
+let nombres =['maria','paola','julia','susana']
+console.log(nombres);
+console.log(nombres.toString()); //cadena
+console.log(nombres.join(''));
+console.log(nombres.join(' '));
+console.log(nombres.join(', '));
+console.log(nombres.join(' 👩🏻 '));
+
+// Metodos mas usados de una arreglo
+
+let personas=['👩🏻','👧🏻','🧒🏻','👨🏻']
+console.log(personas); //
+personas.push('👨🏻‍🦱')
+personas.push('👨🏻‍🦰')
+console.log(personas);
+personas.pop()
+console.log(personas);
+personas.pop()
+console.log(personas);
+personas.shift()
+console.log(personas);
+personas.shift()
+console.log(personas);
+personas.unshift('👸🏻')
+console.log(personas);
+personas.reverse()
+console.log(personas);
+
+let primerosNumeros=[1,2,3]
+let segundosNumeros=[1,4,9]
+
+let arregloDeArreglos= [[1,2,3],[1,4,9]] // 2 arreglo elementos
+console.log(arregloDeArreglos[0]);
+console.log(arregloDeArreglos[1]);
 
 
 
@@ -29,11 +124,38 @@ for (let i = 0; i <= sillas.length-1; i++) { //4
 
 
 
-const arr= Array()
-console.log(arr);
 
-const arr1=[]
-console.log(arr1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ Funciones.js
@@ -0,0 +1,23 @@
+
+function 👩🏻‍🦰('🥝') {
+    console.log('🚿');
+    console.log('💻');
+    console.log('🍟');
+    console.log('Mi primera comida es ' + '🥝');
+    return 'Acabe con las tareas'    
+}
+//
+        CODIGO
+        👩🏻‍🦰('🥝') //String 
+        let repuesta= 👩🏻‍🦰()
+        console.log(respuesta);
+
+//
+function cuadrados() {
+    let numero= 3
+    let c= numero * numero
+    console.log(c);
+}
+cuadrados()
+
+
